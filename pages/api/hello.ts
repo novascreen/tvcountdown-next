5593bace6ad118d6312e2f77f485743be58a824b
Thomas Hermann <1596280+novascreen@users.noreply.github.com>
2021-01-02 16:27:28 -0500

    Add prettier & eslint

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.statusCode = 200;
-  res.json({ name: "John Doe" });
-};
+export default (_req: NextApiRequest, res: NextApiResponse) => {
+  res.statusCode = 200
+  res.json({ name: 'John Doe' })
+}
